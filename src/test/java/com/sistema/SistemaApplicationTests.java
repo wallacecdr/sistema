ce02815a2ce8f5f967f50d9f7285859fa509b88f
Wallace.Caldeira <wallace.cldr@gmail.com>
2019-11-13 17:33:01 -0300

    Criação do projeto

---
+++ src/test/java/com/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
